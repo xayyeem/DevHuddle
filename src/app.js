b35b3942f1663eacad88ccb202eb43341471880c
xayyeem <kjafri30izback@gmail.com>
2024-11-12 12:39:32 +0530

    get post delete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res) => {
-    res.send('hello from server')
+app.get('/user', (req, res) => {
+    res.send({ firstname: 'khalid', lastname: 'khalid' })
+})
+
+app.post('/user', (req, res) => {
+    res.send('saved success')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('deleted success')
+})
+
+app.use('/test', (req, res) => {
+    res.send('hello from test')
 })
 
 app.listen(3000, () => {
